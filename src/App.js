59e468e2d7feab4cf14f82c6b4f6fbf6a757fc52
tankubopa777 <84582195+tankubopa777@users.noreply.github.com>
2023-03-22 22:52:06 +0700

    section 3
    learn how to use toLocaleString

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    {title: 'Toilet Paper', 
+    amount: 94.12, 
+    date: new Date(2021, 2, 28) 
+  },
+    {title: 'Car Insurance', 
+    amount: 294.67, 
+    date: new Date(2021, 4, 28) 
+  },
+    {title: 'Keyborad', 
+    amount: 68.67, 
+    date: new Date(2021, 9, 28) 
+  },
+    {title: 'Pen', 
+    amount: 2.57, 
+    date: new Date(2021, 10, 28) }
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Let's make some fun</h2>
+      <p>This is gonna be fun</p>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].title} date={expenses[0].date}></ExpenseItem>
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].title} date={expenses[1].date}></ExpenseItem>
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].title} date={expenses[2].date}></ExpenseItem>
+      <ExpenseItem title={expenses[3].title} amount={expenses[3].title} date={expenses[3].date}></ExpenseItem>
     </div>
   );
 }
