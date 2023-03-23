fecfae53e81a93cd03201dd11e0764e75148bf81
tankubopa777 <84582195+tankubopa777@users.noreply.github.com>
2023-03-23 12:24:41 +0700

    Section 3
    Custom date month year

--- src/App.js
+++ src/App.js
@@ -22,10 +22,10 @@ function App() {
     <div className="App">
       <h2>Let's make some fun</h2>
       <p>This is gonna be fun</p>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].title} date={expenses[0].date}></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].title} date={expenses[1].date}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].title} date={expenses[2].date}></ExpenseItem>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].title} date={expenses[3].date}></ExpenseItem>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].title} date={expenses[0].date}/>
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].title} date={expenses[1].date}/>
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].title} date={expenses[2].date}/>
+      <ExpenseItem title={expenses[3].title} amount={expenses[3].title} date={expenses[3].date}/>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,11 @@
-import './ExpenseItem.css'
+import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
+    
     return (
     <div className="expense-item">
-        <div>
-            <div>{props.date.toLocaleString('en-US', {month : 'long'})}</div>
-            <div>Year</div>
-            <div>Day</div>
-        </div>
+        <ExpenseDate date = {props.date}/>
         <div className="expense-item__description">
             <h2>{props.title}</h2>
             <div className="expense-item__price">${props.amount}</div>
