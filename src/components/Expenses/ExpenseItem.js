e1c7b68d8a6de7fc7002ee8113af4af879d7ea3b
tankubopa777 <84582195+tankubopa777@users.noreply.github.com>
2023-03-25 15:11:40 +0700

    Section 4
    learn how to create event.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,21 @@
 import './ExpenseItem.css';
+import Card from '../UI/Card'
 import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-    
+    const clickHandler = () => {
+        console.log('Clicke!!!!')
+    };
+
     return (
-    <div className="expense-item">
+    <Card className="expense-item">
         <ExpenseDate date = {props.date}/>
         <div className="expense-item__description">
             <h2>{props.title}</h2>
             <div className="expense-item__price">${props.amount}</div>
         </div>
-    </div>
+        <button onClick={clickHandler}>Chang Title</button>
+    </Card>
     );
 }
 export default ExpenseItem;
